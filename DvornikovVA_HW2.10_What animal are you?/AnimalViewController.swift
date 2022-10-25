7de5adc920c804b8a0ed4bc332b774b6ba254198
Vladimir Dvornikov <echobizarre@gmail.com>
2022-10-25 16:39:48 +0300

    added screens and segues

---
+++ DvornikovVA_HW2.10_What animal are you?/AnimalViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DetailViewController.swift
+//  DvornikovVA_HW2.10_What animal are you?
+//
+//  Created by Vladimir Dvornikov on 25.10.2022.
+//
+
+import UIKit
+
+class AnimalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
